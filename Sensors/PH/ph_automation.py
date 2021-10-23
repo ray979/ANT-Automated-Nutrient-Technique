eacf8540abc6c88d0da5ce89ba2e9f95877e21a2
Raymond Duong <raymondd0787@gmail.com>
2021-10-23 10:14:43 -0700

    update on ph automation

--- Sensors/PH/ph_automation.py
+++ Sensors/PH/ph_automation.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+from Waveshare_AD import phsensor
 import sys
 import time
 
@@ -16,20 +17,35 @@ def GPIOSetup():
 if __name__ == '__main__':
     try:
         GPIOSetup()
+        ph_sensor = phsensor.PHSensor(0,14) #phsensor object
         while True:
             #ph sensing from ad da board
+            ph = ph_sensor.read_ph(0)
 
             #if ph level is below range
-            GPIO.output(PH_UP, GPIO.HIGH)
-            #time.sleep(pump duration)
-            GPIO.output(PH_UP, GPIO.LOW)
+            if ph < PH_MIN:
+                #turn on PH up pump
+                GPIO.output(PH_UP, GPIO.HIGH)
+                print(f"PH UP pump on at PH:{ph}")
+                #time.sleep(pump duration)
+                time.sleep(1)
+                #turn off PH up pump
+                GPIO.output(PH_UP, GPIO.LOW)
+                print("PH UP pump off")
 
             #if ph level is above range
-            GPIO.output(PH_DOWN, GPIO.HIGH)
-             #time.sleep(pump duration)
-            GPIO.output(PH_DOWN, GPIO.LOW)
+            if ph > PH_MIN:
+                #turn on PH down pump
+                GPIO.output(PH_DOWN, GPIO.HIGH)
+                print(f"PH DOWN pump on at PH:{ph}")
+                #time.sleep(pump duration)
+                time.sleep(1)
+                #turn off PH down pump
+                GPIO.output(PH_DOWN, GPIO.LOW)
+                print("PH DOWN pump off")
 
             #ph sensing interval
+            time.sleep(0.5)
     except KeyboardInterrupt:
         GPIO.cleanup()
         sys.exit('PH balancing ended')
