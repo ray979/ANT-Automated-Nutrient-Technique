54b963b5168ace923a2ca2288cb2d8d468bc6a1b
Raymond Duong <raymondd0787@gmail.com>
2022-02-14 15:44:48 -0800

    Separate folder for web app

---
+++ ANT-WEB-APP/main.py
@@ -0,0 +1,63 @@
+import datetime
+import RPi.GPIO as GPIO
+import paho.mqtt.client as mqtt
+import threading
+
+import uvicorn
+from fastapi import FastAPI
+from starlette.templating import Jinja2Templates
+from starlette.requests import Request
+
+GPIO.setmode(GPIO.BCM)
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+jinja_env = templates.env
+
+PH = 0
+PH_TOPIC = 'sensor/ph'
+
+lock = threading.Lock()
+
+def on_message(client, userdata, message):
+    global PH
+    if(message.topic == PH_TOPIC):
+        print(f'The message is {str(message.payload.decode("utf-8"))}')
+        lock.acquire()
+        PH = round(float(str(message.payload.decode("utf-8"))),2)
+        lock.release()
+
+
+
+client = mqtt.Client("ANT System Web API")
+client.on_message = on_message
+client.connect("localhost", 1883)
+client.subscribe(PH_TOPIC)
+client.loop_start()
+
+
+@app.get("/")
+def root(request: Request):
+    return templates.TemplateResponse('index.html',
+                                      {'request': request})
+
+@app.get("/sensordata")
+def returndata():
+    """Returns timestamp and PH value from MQTT"""
+    try:
+        today = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        ph = PH
+        dat = {"time": today, "PH": PH}
+        print(f"json: {dat}")
+        return dat
+    except Exception as e:
+        print(f"err: {str(e)}")
+
+    return None
+
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+    client.loop_stop()
