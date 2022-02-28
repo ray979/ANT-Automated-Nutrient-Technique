a326f53df35ffe14befcd0f31a4b027bc7901996
ray979 <81543048+ray979@users.noreply.github.com>
2022-02-27 18:21:45 -0800

    Delete ANT-WEB-APP/ANT-FASTAPI-BACKEND directory

--- ANT-WEB-APP/ANT-FASTAPI-BACKEND/config/database.py
+++
@@ -1,6 +0,0 @@
-#import statements
-from pymongo import MongoClient
-
-#create BD connection
-#connection = MongoClient("mongodb://localhost:27017/test")
-connection = MongoClient()
--- ANT-WEB-APP/ANT-FASTAPI-BACKEND/models/sensordata.py
+++
@@ -1,6 +0,0 @@
-#import statements
-from pydantic import BaseModel
-
-class SensorData(BaseModel):
-    ant_ph: float
-    ant_ec: float
