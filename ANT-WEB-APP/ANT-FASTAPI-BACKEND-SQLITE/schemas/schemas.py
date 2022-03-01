d28fff0a60f21b3ac8c5dcd4e7fa89d5d342ba06
ray979 <81543048+ray979@users.noreply.github.com>
2022-03-01 11:37:57 -0800

    Added new comments

--- ANT-WEB-APP/ANT-FASTAPI-BACKEND-SQLITE/schemas/schemas.py
+++ ANT-WEB-APP/ANT-FASTAPI-BACKEND-SQLITE/schemas/schemas.py
@@ -1,8 +1,10 @@
-#schemas helps to serailize and also convert sqlite format json to our UI needed json
+#schemas helps to serailize and also convert sqlite format json to the format our React frontend will read
 
+#import statements
 from pydantic import BaseModel, Field
 import datetime
 
+#convert database object to dictionary output
 def sensorDataEntity(db_item) -> dict:
     return {
         "id": db_item.id,
@@ -14,6 +16,7 @@ def sensorDataEntity(db_item) -> dict:
 
     }
 
+#convert list of database object to list of sensordataentity
 def listOfSensorDataEntity(db_item_list) -> list:
     list_sens_entity = []
     for item in db_item_list:
@@ -21,13 +24,17 @@ def listOfSensorDataEntity(db_item_list) -> list:
 
     return list_sens_entity
 
+#Schemas for specifying the specific format and types of values that can used in post requests
+#format for entering sensordata into post request
 class SensorData(BaseModel):
     ph: float = Field(ge = 0, le = 14)
     ec: float
 
+#format for passing a new sensordata for creating
 class SensorDataCreate(SensorData):
     pass
 
+#format for passing settings values into post
 class Settings(BaseModel):
     phmin:float = Field(ge = 0)
     phmax:float = Field(le = 14)
