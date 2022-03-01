c9c7d3726566e8acac8dcfd598184abd24ebbfad
ray979 <81543048+ray979@users.noreply.github.com>
2022-03-01 11:30:48 -0800

    Added new comments

--- ANT-WEB-APP/ANT-FASTAPI-BACKEND-SQLITE/models/models.py
+++ ANT-WEB-APP/ANT-FASTAPI-BACKEND-SQLITE/models/models.py
@@ -1,13 +1,14 @@
+#import statements
 import sqlalchemy as _sql
 import sqlalchemy.orm as _orm
 import datetime as _dt
-
 from config.database import Base
 
+#database model for sensor data
 class SensorData(Base):
     __tablename__ = "sensor_reading"
-    id = _sql.Column(_sql.Integer, primary_key=True, index=True)
+    id = _sql.Column(_sql.Integer, primary_key=True, index=True) #id will be index of row added
     ph_reading = _sql.Column(_sql.Float)
     ec_reading = _sql.Column(_sql.Float)
-    date_posted =  _sql.Column(_sql.Date, default=_dt.date.today)
-    datetime_posted = _sql.Column(_sql.DateTime, default=_dt.datetime.now)
+    date_posted =  _sql.Column(_sql.Date, default=_dt.date.today) #database will automatically create and save the date row added
+    datetime_posted = _sql.Column(_sql.DateTime, default=_dt.datetime.now) #database will automatically save the datetime added
